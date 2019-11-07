6478eedf4acb836b056e6e20dfcb63e1bcf7dc72
粟慧军 <123>
2019-11-07 20:07:45 +0800

    add

---
+++ 移动开发/b1/build/precache-manifest.8c33bdfcf2ae22d7004b0d533edc4f34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257cc74e78f130e3965646ac1918d6f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53b01b98aa26d6bd9c9",
+    "url": "/static/css/main.614d274c.chunk.css"
+  },
+  {
+    "revision": "6138ada739fe1bb87f87",
+    "url": "/static/js/2.a06d8e58.chunk.js"
+  },
+  {
+    "revision": "d53b01b98aa26d6bd9c9",
+    "url": "/static/js/main.0d7dab97.chunk.js"
+  },
+  {
+    "revision": "f8753c12d00e9c027edd",
+    "url": "/static/js/runtime-main.f670cac5.js"
+  }
+]);
